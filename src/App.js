bff94f47d29dc520edc69278993b864f1a38682b
Mateo-Rey <jrey3darts@gmail.com>
2023-03-23 09:40:50 -0400

    styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='flex'>
+    <div className="w-[20%] h-full absolute bg-gradient-to-br from-sidebar-primary to-sidebar-secondary shadow-sidebar-shadow shadow-2xl">
+      
     </div>
+    <div className='w-[80%] h-full bg-gradient-to-br from-soft-light-blue to-gradient-blue absolute left-[20%] shadow-shadow-blue shadow-lg'>
+    <div className='absolute h-[15%] rounded-t-lg bottom-0 opacity-50 shadow-t-lg w-full bg-black'>input</div>
+    </div>
+    </div>
+    </>
   );
 }
 
