ee047db4783f7aa002f6d38dd808f164d578cdf2
Mateo-Rey <jrey3darts@gmail.com>
2023-03-23 17:31:08 -0400

    swap widget, input box, side bar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
-import './App.css';
+import "./App.css";
+import { ethers } from "ethers";
+import { SwapWidget } from "@uniswap/widgets";
+import "@uniswap/widgets/fonts.css";
 
 function App() {
+  const theme = {
+    primary: "#FFF",
+    secondary: "#A9A9A9",
+    interactive: "#000",
+    container: "#3E517A",
+    module: "#b2f3",
+    accent: "#71FF98",
+    outline: "#42F2F7",
+    dialog: "#000",
+    fontFamily: "Nunito",
+    borderRadius: 1,
+  };
+  const JSONRPCMAP = {
+    1: ["https://mainnet.infura.io/v3/20034e7b0b2d4dbda836cb13cb819bb4"],
+  };
   return (
     <>
-    <div className='flex'>
-    <div className="w-[20%] h-full absolute bg-gradient-to-br from-sidebar-primary to-sidebar-secondary shadow-sidebar-shadow shadow-2xl">
-      
-    </div>
-    <div className='w-[80%] h-full bg-gradient-to-br from-soft-light-blue to-gradient-blue absolute left-[20%] shadow-shadow-blue shadow-lg'>
-    <div className='absolute h-[15%] rounded-t-lg bottom-0 opacity-50 shadow-t-lg w-full bg-black'>input</div>
-    </div>
-    </div>
+      <div className="flex">
+        <div className="w-[20%] h-full absolute bg-gradient-to-br from-sidebar-secondary to-sidebar-primary shadow-2xl">
+          <SwapWidget className="z-10 drop-shadow-2xl" width="20%" brandedFooter={false} jsonRpcUrlMap={JSONRPCMAP} theme={theme} />
+        </div>
+        <div className="w-[80%] h-full bg-gradient-to-br from-soft-light-blue to-gradient-blue absolute left-[20%] shadow-shadow-blue shadow-lg">
+          <div className="absolute h-[10%] brightness-150 shadow-lg drop-shadow-md rounded-full bottom-0 bg-opacity-60 w-full bg-slate-600">
+            <input className="outline-none p-6 font-nunito h-[90%] shadow-lg rounded-full drop-shadow-md text-2xl text-white bg-transparent relative top-0.5 left-3 backdrop-blur-lg text-center w-[98%]" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,15 +14,17 @@ module.exports = {
         "t-2xl": "0 -25px 50px -12px rgba(0, 0, 0, 0.25)",
         "t-3xl": "0 -35px 60px -15px rgba(0, 0, 0, 0.3)",
         "b-sm": "0 1px -2px 0 rgba(0, 0, 0, 0.05)",
-        "b-md": "0 4px -6px 1px rgba(0,0,0,0.1), 0 -2px -4px 1px rgba(0,0,0,0.6)"
+        "b-md":
+          "0 4px -6px 1px rgba(0,0,0,0.1), 0 -2px -4px 1px rgba(0,0,0,0.6)",
       },
+      fontFamily: { 'nunito': ["nunito", "sans-serif"] },
       colors: {
         "soft-light-blue": "#a1b7ff",
         "gradient-blue": "#879ae4",
         "shadow-blue": "#8091d7",
         "light-shadow-blue": "#adc5ff",
-        "sidebar-secondary": "#9ab0be",
-        "sidebar-primary": "#b7d2e2",
+        "sidebar-secondary": "#bbc8ff",
+        "sidebar-primary": "#8b94d9",
         "sidebar-shadow": "#858a99",
         "page-bgcolor": "#faffff",
         "page-accent": "#d3e0e2",
