fffe508ec6a58f84a7a3f2b2f0a91e6436071957
Mateo-Rey <jrey3darts@gmail.com>
2023-03-25 12:56:51 -0400

    message feed

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import "./App.css";
 import { useState, useEffect } from "react";
 import { SwapWidget } from "@uniswap/widgets";
 import "@uniswap/widgets/fonts.css";
-import cryptoavatar from "./assets/cryptoavatar.jpg";
+import { ChatMessage } from "./components/ChatMessage";
 function App() {
   const [models, setModels] = useState([]);
+  const [keyPress, setKeyPress] = useState();
   const [activeModel, setActiveModel] = useState("gpt-3.5-turbo");
   const [user, setUser] = useState("user");
   const [question, setQuestion] = useState("");
@@ -31,26 +32,33 @@ function App() {
   console.log(question);
   const handleSubmit = async (e) => {
     e.preventDefault();
-    setMessageList([...messageList, { role: user, content: question }]);
+    let chatListNew = [...messageList, {role: "user", content: `${question}`}]
     setQuestion("");
     setLoading(true);
-    const messages = messageList;
+    setMessageList(chatListNew);
     const response = await fetch("http://localhost:3012/cryptochat", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "true",
-      },
-      body: JSON.stringify({
-        messages: messages,
-        model: activeModel,
-        max_tokens: tokens,
-        temperature: temperature,
-      }),
-    });
-    console.log(response.json());
-    setMessageList([...messageList, { name: "ai", message: response.message }]);
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "true",
+        },
+        body: JSON.stringify({
+          messages: chatListNew,
+          model: activeModel,
+          max_tokens: tokens,
+          temperature: temperature,
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) =>
+          setMessageList([
+            ...chatListNew,
+            { role: "assistant", content: data.message },
+          ])
+        );
+    setLoading(false);
   };
+  console.log(messageList);
   useEffect(() => {
     fetch("http://localhost:3012/models")
       .then((response) => response.json())
@@ -65,7 +73,7 @@ function App() {
       <div className="h-[100%] w-[100%] flex flex-col absolute bg-transparent overflow-hidden">
         <div className="md:w-[25%] h-[40%] md:h-full md:absolute bg-gradient-to-bl from-sidebar-secondary to-sidebar-primary shadow-2xl">
           <button
-            className="effect effect-inner drop border-2 active:border-4 active:effect-smaller-inner active:ring-slate-400 py-2 rounded-3xl bg-slate-400 w-36 h-10"
+            className="effect text-center text-white effect-inner active:effect-smaller-inner active:border-2 py-2 rounded-3xl bg-chat-primary w-36 h-10"
             onClick={() => {
               setWidgetShow(!widgetShow);
             }}
@@ -88,7 +96,7 @@ function App() {
                   />
                 </div>
                 <button
-                  className="effect effect-inner drop border-2 active:border-4 active:effect-smaller-inner active:ring-slate-400 py-2 rounded-3xl bg-slate-400 w-36 h-10"
+                  className="effect text-center text-white effect-inner active:effect-smaller-inner active:border-2 py-2 rounded-3xl bg-chat-primary w-36 h-10"
                   onClick={() => {
                     setWidgetShow(!widgetShow);
                   }}
@@ -99,44 +107,19 @@ function App() {
             )}
           </div>
         </div>
-        <div className="md:w-[75%] h-full overflow-hidden bg-gradient-to-br from-soft-light-blue to-gradient-blue md:absolute left-[25%] shadow-shadow-blue shadow-lg">
+        <div className="md:w-[75%] h-full overflow-scroll bg-gradient-to-br from-chat-primary to-chat-secondary md:absolute left-[25%] shadow-light-chat-shadow shadow-lg">
           <div className="overflow-scroll flex flex-col place-items-center h-[90%]">
-            {messageList.map((message) => {
-              if (message.name === "ai") {
-                return (
-                  <>
-                    <div className="outline p-2 my-2 text-white outline-4 outline-input-outline bg-input-primary bg-opacity-80 ease-in-out shadow-lg rounded-lg">
-                      <div className="flex flex-col items-center">
-                        <div className="flex items-center space-x-3">
-                          <img className="rounded-full" src={cryptoavatar} />
-                          <div className="font-nunito">{message.name}</div>
-                        </div>
-                        <div className="font-nunito">{message.message}</div>
-                      </div>
-                    </div>
-                  </>
-                );
-              } else {
-                return (
-                  <>
-                    <div className="outline text-white p-2 my-2 outline-4 outline-input-outline bg-input-primary bg-opacity-80 ease-in-out shadow-lg rounded-lg">
-                      <div className="flex flex-col items-center">
-                        <div className="flex items-center space-x-3">
-                          <div className="font-nunito">{message.name}</div>
-                        </div>
-                        <div className="font-nunito">{message.message}</div>
-                      </div>
-                    </div>
-                  </>
-                );
-              }
-            })}
+            {messageList.map((message, i) => (
+                <ChatMessage message={message} key={i} />
+              ))};
+          
           </div>
           <div className="absolute h-[10%] shadow-lg drop-shadow-md rounded-full bottom-0 bg-opacity-60 w-full bg-slate-600">
             <form onSubmit={(e) => handleSubmit(e)}>
               <input
                 name="input"
                 onChange={(e) => setQuestion(e.target.value)}
+                onKeyDown={(e) => setKeyPress(e.key)}
                 value={question}
                 className="outline-none tracking-wide p-6 font-nunito h-[85%] md:h-[90%] shadow-lg rounded-full drop-shadow-md text-2xl text-white bg-transparent relative top-1 md:top-0.5 left-1 md:left-3 backdrop-blur-lg text-center w-[98%]"
               />
